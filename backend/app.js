9828d470706a70454938e20f574ec5364bead794
1mtuna <dwlee9409@gmail.com>
2022-06-23 13:38:57 +0900

    test

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example main listening at http://localhost:${port}`))
+
+//
+//
+//
+
+var jsonText = '{ "name": "Someone else", "lastName": "Kim" }';  // JSON 형식의 문자열
+var realObject = JSON.parse(jsonText);
+var jsonText2 = JSON.stringify(realObject);
+
+console.log(realObject);
+console.log(jsonText2);
